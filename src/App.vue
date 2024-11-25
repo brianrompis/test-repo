29c770d90177ced6d3f2a41037c586b169e8cb12
brianrompis <brian.rompis@gmail.com>
2024-11-25 17:08:19 +0800

    change title

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Etika Profesi" />
+  <HelloWorld msg="Teamwork: Tatap Muka 3" />
 </template>
 
 <style scoped>
