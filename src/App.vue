b306b925fd39a848f94916523aaa166fe4f6901b
brianrompis <brian.rompis@gmail.com>
2024-12-02 15:51:13 +0800

    rubah text

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Teamwork: Tatap Muka 3" />
+  <HelloWorld msg="Teamwork: Tatap Muka 4 branch B" />
 </template>
 
 <style scoped>
